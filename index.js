22c8304ecaf845fbed101ad14b3b5690709db781
Ujwal Gouda <97618553+ugouda@users.noreply.github.com>
2023-11-27 23:09:57 +0530

    index.js

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ Array.from(buttons).forEach((button)=>{
     button.addEventListener('click', (e)=> {
         if(e.target.innerHTML == '='){
             string = eval(string)
-            string = string.toFixed(8)
             texts.innerHTML = string;
-            console.log(string)
         }else if(e.target.innerHTML == 'AC'){
             string = ""
             texts.innerHTML = string;
@@ -20,7 +18,6 @@ Array.from(buttons).forEach((button)=>{
             string = string*-1;
             texts.innerHTML = string;
         }else{
-            console.log(e.target)
             string = string + e.target.innerHTML;
             texts.innerHTML = string;
         }
